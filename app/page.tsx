17f890abd2dab383421279de8e3eeac4f4ec16fc
Megan Pearson <megan.e.pearson@gmail.com>
2026-01-23 15:30:14 +0100

    Sets fixed width; updates html, some copy tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,50 @@
 "use client";
 
-import { Experience } from "@/components/Experience";
 import { useState } from "react";
 
 export default function Home() {
   const [showCV, setShowCV] = useState(false);
 
   return (
-    <div className="w-100">
+    <div className="w-[500]">
       <p className="bold-text">Hi, I’m Megan Pearson</p>
       <p>
-        I’ve been building software for 11 years. For the last several years, I’ve been leading teams to do so.
+        I’ve been building software for more than a decade. For the last several
+        years, I’ve been leading teams to do so.
       </p>
       <p>
         Previously, I’ve led product and engineering at{" "}
         <a href="https://www.raycast.com/">Raycast</a>,{" "}
         <a href="https://pitch.com/">Pitch</a>, and design studio{" "}
-        <a href="https://bakkenbaeck.com/">Bakken & Bæck</a>. 
-        
-        Most recently, at Raycast, I led AI & iOS, where I directed the development of AI infrastructure to deliver reliable, performant AI features—including evaluation systems, feedback pipelines, and automated model integration. 
-        <br />
-        <br />
-        I drove significant reliability improvements while conducting user research that shaped our broader AI product strategy, and evolved AI from exploratory features to production-grade systems (20% paid adoption, 108% reliability improvement). Guiding the team that shipped our iOS app, it became <em>App of the Day</em>&nbsp; in all markets. And, I scaled our backend teams 3<span className="text-xs align-middle ps-[2px]">×</span>. 
-        <br />
-        <br />
-        Before working in tech, I was a pilot and flight instructor teaching students to fly and prepare for private and commerical pilot certificates.
-        <br />
-        <br />
-        When I’m not working, I’m often running: a 2<span className="text-xs align-middle ps-[2px]">×</span> marathoner, I ran marathons in Amsterdam (2024) and Berlin (2025).{" "}
+        <a href="https://bakkenbaeck.com/">Bakken & Bæck</a>. Most recently, at
+        Raycast, I led AI & iOS, where I directed the development of AI
+        infrastructure to deliver reliable, performant AI features—including
+        evaluation systems, feedback pipelines, and automated model integration.
+      </p>
+      <p>
+        I drove significant reliability improvements while conducting user
+        research that shaped our broader AI product strategy, and evolved AI
+        from exploratory features to production-grade systems (20% paid
+        adoption, 108% reliability improvement). Guiding the team that shipped
+        our iOS app, it became <em>App of the Day</em>
+        &nbsp; in all markets. And, I scaled our backend teams 3
+        <span className="text-xs align-middle ps-[2px]">×</span>.
+      </p>
+      <p>
+        Before working in tech, I was a pilot and flight instructor teaching
+        students to fly and prepare for private and commerical pilot
+        certificates.
+      </p>
+      <p>
+        When not working, I’m often running: a 2
+        <span className="text-xs align-middle ps-[2px]">×</span> marathoner, I
+        ran marathons in Amsterdam (2024) and Berlin (2025).{" "}
       </p>
 
       <div className="contact-section">
         <p>
           <span className="inline-block w-2 h-2 rounded-full bg-green-500 mr-2 align-middle"></span>
-          Currently available for work
+          Available for work Feb 2026
         </p>
         <p>
           <a
@@ -41,17 +52,21 @@ export default function Home() {
             href="https://www.linkedin.com/in/meganepearson/"
             target="_blank"
           >
-            Find my resumé on LinkedIn
+            Find my CV on LinkedIn
           </a>
-          <a className="block" href="mailto:megan.e.pearson@gmail.com" target="_blank">
+          <a
+            className="block"
+            href="mailto:megan.e.pearson@gmail.com"
+            target="_blank"
+          >
             Send me an email
           </a>
         </p>
-        
-        { /* <p>
+
+        {/* <p>
           <button onClick={() => setShowCV(!showCV)}>CV</button>
         </p>
-        <div>{showCV && <Experience />}</div>*/ }
+        <div>{showCV && <Experience />}</div>*/}
       </div>
 
       {/* <div>
